e4b9db6005a5bc083f2f9b1610698dad5b4dc7a7
oliverellendavis <oliverellendavis@gmail.com>
2024-01-28 13:49:38 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string Name = "Oliver";
+            int Age = 29;
+            char lastInitial = 'D';
+            bool isOliverTired = true;
+            double hoursOfSleep = 6.5;
+            decimal costOfLatte = 5m; 
+
+            Console.WriteLine($"My name is {Name}, and I'm {Age} years old. My last initial is {lastInitial}, and it's {isOliverTired} that I'm tired, though I got {hoursOfSleep} last night. Hopefully a latte will wake me up, even though it costs {decimal} dollars.");
+            
         }
     }
 }
